eb0496ef1075b3225b8f93886dcc0e2c1ab4408f
mmaheen <mushrifmaheen@gmail.com>
2025-08-07 04:05:50 +0600

    User image

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $faker = \Faker\Factory::create();
+
+        $source_path = public_path('assets/frontend/img/person');
+        $destination_path = public_path('uploads/users');
+
+        File::cleanDirectory($destination_path);
+        File::copyDirectory($source_path, $destination_path);
+
+        foreach (range(1, 10) as $index) {
+            $images = File::files($destination_path);
+            $random_image = $images[array_rand($images)];
+            $image_name = $random_image->getFilename();
+            
+            User::factory()->create([
+                'name' => $faker->name(),
+                'email' => $faker->unique()->safeEmail(),
+                'password' => $faker->password(),
+                'image'=> $image_name,
+                'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+            ]);
+        }
+    }
+}

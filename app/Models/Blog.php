09b5364e3cc82eab741b44f954fa025d42edd4f2
mmaheen <mushrifmaheen@gmail.com>
2025-08-27 00:03:41 +0600

    Protected fillable in models

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'subtitle',
+        'description',
+        'image',
+        'slug',
+        'is_published',
+        'user_id',
+        'category_id',
+    ];
+
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'slug',
+        'is_published',
+        'user_id',
+        'category_id',
+    ];
 }

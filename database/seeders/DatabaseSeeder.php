8f8b0ac490e20a540dc4ea9afdea24e34928c4ad
mmaheen <mushrifmaheen@gmail.com>
2025-08-29 22:31:50 +0600

    Testimonial model, migration, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             BlogTagSeeder::class,
             SkillSeeder::class,
             TeamSeeder::class,
+            TestimonialSeeder::class,
         ]);
     }
 }

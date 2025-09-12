2353c10e3950b6876c63f448e13e2f286fb4ab86
mmaheen <mushrifmaheen@gmail.com>
2025-09-12 15:04:57 +0600

    User & Blog relation

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,7 +8,10 @@ class Blog extends Model
 {
     //
     protected $fillable = ['title', 'subtitle', 'slug', 'description', 'image', 'is_published', 'user_id', 'category_id'];
-
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
     public function category()
     {
         return $this->belongsTo(Category::class);
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }

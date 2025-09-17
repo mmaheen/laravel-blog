79b5f83f1d90d7f690d5cb440b1180d2eb7408ca
mmaheen <mushrifmaheen@gmail.com>
2025-09-17 20:23:33 +0600

    Blog active in Header

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -15,7 +15,10 @@
                     <li><a href="#portfolio">Photos</a></li>
                     <li><a href="#recent-blog-postst">Recent Blogs</a></li>
                 @endif
-                <li><a href="{{ route('blog.index') }}">Blogs</a></li>
+                <li><a href="{{ route('blog.index') }}"
+                        class = "{{ Route::currentRouteName() == 'blog.index' ? 'active' : '' }}">
+                        Blogs
+                    </a></li>
                 <li class="dropdown"><a href="#"><span>Authentication</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>

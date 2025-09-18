0d0ecf8b2cfe5c8c02c1d2581a81302763531b42
mmaheen <mushrifmaheen@gmail.com>
2025-09-19 03:06:17 +0600

    Header for authentication

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -19,16 +19,20 @@
                         class = "{{ Route::currentRouteName() == 'blog.index' ? 'active' : '' }}">
                         Blogs
                     </a></li>
-                <li class="dropdown">
-                    <a href="#"
-                        class = "{{ Route::currentRouteName() == 'login' || Route::currentRouteName() == 'register' ? 'active' : '' }}">
-                        <span>Authentication</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
-                    </a>
-                    <ul>
-                        <li><a href="{{ route('login.form') }}">Login</a></li>
-                        <li><a href="{{ route('register.form') }}">Register</a></li>
-                    </ul>
-                </li>
+                @guest
+                    <li class="dropdown">
+                        <a href="#"
+                            class = "{{ Route::currentRouteName() == 'login' || Route::currentRouteName() == 'register' ? 'active' : '' }}">
+                            <span>Authentication</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
+                        </a>
+                        <ul>
+                            <li><a href="{{ route('login.form') }}">Login</a></li>
+                            <li><a href="{{ route('register.form') }}">Register</a></li>
+                        </ul>
+                    </li>
+                @else
+                    <li><a href="{{ route('dashboard') }}">{{ Auth::user()->name }}</a></li>
+                @endguest
 
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
